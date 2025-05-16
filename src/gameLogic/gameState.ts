c32b3bbb3664f6574dc3797c9b18d874601a5bae
Nikola Kenjic <nikola.nico89@gmail.com>
2025-05-16 20:49:07 +0200

    create calculate score function

--- src/gameLogic/gameState.ts
+++ src/gameLogic/gameState.ts
@@ -13,6 +13,34 @@ function dealCard(deck: Card[], player: Player) {
   player.hand.push(card);
 }
 
+function calculateScore(hand: Card[]): number {
+  let score = 0;
+  let aceCount = 0;
+
+  // go through the cards
+  for (const card of hand) {
+    if (card.value === 'Ace') {
+      aceCount++;
+    } else if (['Jack', 'Queen', 'King'].includes(card.value)) {
+      score += 10;
+    } else {
+      score += parseInt(card.value);
+    }
+  }
+
+  //   Add "Aces" to score
+  for (let i = 0; i < aceCount; i++) {
+    // If you can add 11 without going over 21, add 11, otherwise add 1
+    if (score + 11 <= 21) {
+      score += 11;
+    } else {
+      score += 1;
+    }
+  }
+
+  return score;
+}
+
 function dealInitialCards(deck: Card[]): {
   remainingDeck: Card[];
   player: Player;
