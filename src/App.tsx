f1fe72b9fc6fdd0c39f176b14473ba043ffde39c
Nikola Kenjic <nikola.nico89@gmail.com>
2025-05-22 20:32:10 +0200

    initial ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
+import { useEffect, useState } from 'react';
+import { startNewGame } from './gameLogic/gameController';
+import type { Player } from './gameLogic/gameState';
+
 function App() {
+  const [player, setPlayer] = useState<Player | null>(null);
+  const [dealer, setDealer] = useState<Player | null>(null);
+
+  useEffect(() => {
+    const { player, dealer } = startNewGame();
+    setPlayer(player);
+    setDealer(dealer);
+  }, []);
+
+  if (!player || !dealer) return <h1>Loading...</h1>;
+
   return (
-    <>
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Hello world!
+    <div className="p4 flex justify-center items-center flex-col">
+      <h1 className="font-bold text-3xl mb-6 text-center mt-3">
+        Blackjack Game
       </h1>
-    </>
+
+      <div className="mb-4">
+        <h2 className="text-xl font-semibold">Player</h2>
+        <p>Score: {player.score}</p>
+      </div>
+
+      <div>
+        <h2 className="text-xl font-semibold">Dealer</h2>
+        <p>Score: {dealer.score}</p>
+      </div>
+    </div>
   );
 }
 
