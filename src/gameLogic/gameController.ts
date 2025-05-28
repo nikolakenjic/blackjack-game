4493a1ccb05a48fc1a5ad832a9031872805031c7
Nikola Kenjic <nikola.nico89@gmail.com>
2025-05-28 17:27:36 +0200

    game controller logic

--- src/gameLogic/gameController.ts
+++ src/gameLogic/gameController.ts
@@ -1,5 +1,5 @@
-import { createDeck } from './deck';
-import { dealInitialCards } from './gameState';
+import { createDeck, type Card } from './deck';
+import { calculateScore, dealInitialCards, type Player } from './gameState';
 
 export function startNewGame() {
   const deck = createDeck();
@@ -8,4 +8,34 @@ export function startNewGame() {
   const { player, dealer, remainingDeck } = dealInitialCards(deck);
 
   return { player, dealer, remainingDeck };
+}
+
+export function handlePlayerHit(player: Player, deck: Card[]) {
+  const newCard = deck[0];
+  const newDeck = deck.slice(1);
+
+  const updateHand = [...player.hand, newCard];
+  const updateScore = calculateScore(updateHand);
+
+  return {
+    updatedPlayer: { ...player, hand: updateHand, score: updateScore },
+    updatedDeck: newDeck,
+  };
+}
+
+export function handleDealerTurn(dealer: Player, deck: Card[]) {
+  const newDeck = [...deck];
+
+  const updatedDealer = { ...dealer };
+  while (calculateScore(updatedDealer.hand) < 17 && newDeck.length > 0) {
+    const card = newDeck.shift();
+    if (card) updatedDealer.hand.push(card);
+  }
+
+  updatedDealer.score = calculateScore(updatedDealer.hand);
+
+  return {
+    updatedDealer,
+    updatedDeck: newDeck,
+  };
 }
