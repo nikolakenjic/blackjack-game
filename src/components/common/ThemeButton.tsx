ffa1fa8608417bf0444e1f6d31ee5acad8ffff50
Nikola Kenjic <nikola.nico89@gmail.com>
2025-06-02 20:21:01 +0200

    add active class for toggle button

--- src/components/common/ThemeButton.tsx
+++ src/components/common/ThemeButton.tsx
@@ -3,12 +3,15 @@ import type React from 'react';
 interface ThemeButtonProps {
   icon: React.ElementType;
   onClick: () => void;
+  active?: boolean;
 }
 
-const ThemeButton = ({ icon: Icon, onClick }: ThemeButtonProps) => {
+const ThemeButton = ({ icon: Icon, onClick, active }: ThemeButtonProps) => {
   return (
     <button
-      className="bg-transparent hover:bg-zinc-200 dark:hover:bg-zinc-100/20 rounded-lg p-3 cursor-pointer"
+      className={`bg-transparent hover:bg-zinc-200 dark:hover:bg-zinc-100/20 rounded-lg p-3 cursor-pointer ${
+        active ? 'bg-zinc-300 dark:bg-zinc-600' : ''
+      }`}
       onClick={onClick}
     >
       <Icon />
--- src/components/layout/ThemeToggle.tsx
+++ src/components/layout/ThemeToggle.tsx
@@ -9,12 +9,18 @@ interface ThemeToggleProps {
 }
 
 const ThemeToggle = ({ theme, setTheme }: ThemeToggleProps) => {
-  // todo add dark/light theme active for theme props...
-
   return (
     <div className="bg-zinc-100 dark:bg-zinc-700 p-2 rounded-xl absolute top-5 right-5 flex gap-2">
-      <ThemeButton icon={LuSun} onClick={() => setTheme(THEMES.LIGHT)} />
-      <ThemeButton icon={LuMoon} onClick={() => setTheme(THEMES.DARK)} />
+      <ThemeButton
+        icon={LuSun}
+        onClick={() => setTheme(THEMES.LIGHT)}
+        active={theme === THEMES.LIGHT}
+      />
+      <ThemeButton
+        icon={LuMoon}
+        onClick={() => setTheme(THEMES.DARK)}
+        active={theme === THEMES.DARK}
+      />
     </div>
   );
 };
