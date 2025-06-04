23d34ef5cf0543e8dadadb22b0b77fa598aa7aaf
Nikola Kenjic <nikola.nico89@gmail.com>
2025-06-04 20:06:57 +0200

    install toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { useEffect, useReducer } from 'react';
+import { useReducer } from 'react';
 import { useTheme } from './hooks/useTheme';
 import ThemeToggle from './components/layout/ThemeToggle';
 import Header from './components/layout/Header';
 import GameBoard from './components/game/GameBoard';
 import { gameReducer, initialState } from './logic/gameReducer';
-import { checkWinner, isBusted } from './logic/gameController';
+import { useInitializeGame } from './hooks/useInitializeGame';
+import { useGameResult } from './hooks/useGameResult';
+import { useGameActions } from './hooks/useGameActions';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   const { theme, setTheme } = useTheme();
@@ -12,57 +15,30 @@ function App() {
 
   const { player, dealer, result, gameOver, stats } = state;
 
+  const { onHit, onStand, onReset, onResetStats } = useGameActions(
+    dispatch,
+    gameOver
+  );
+
   // // Initialize game
-  useEffect(() => {
-    const storedStats = localStorage.getItem('stats');
-    if (storedStats) {
-      dispatch({ type: 'LOAD_STATS', payload: JSON.parse(storedStats) });
-    }
-
-    dispatch({ type: 'NEW_GAME' });
-  }, []);
-
-  const onHit = () => {
-    if (!gameOver) dispatch({ type: 'PLAYER_HIT' });
-  };
-  const onStand = () => {
-    if (!gameOver) dispatch({ type: 'PLAYER_STAND' });
-  };
-  const onReset = () => {
-    dispatch({ type: 'NEW_GAME' });
-    dispatch({ type: 'RESET_GAME_OVER' });
-  };
-
-  const onResetStats = () => {
-    dispatch({ type: 'RESET_STATS' });
-  };
+  useInitializeGame(dispatch);
 
   // When game ends, delay showing the final result by 1 second
-  useEffect(() => {
-    if (!gameOver) return;
-
-    const timeOutId = setTimeout(() => {
-      if (!player || !dealer) return;
-
-      if (isBusted(player.score)) {
-        dispatch({
-          type: 'SET_RESULT',
-          payload: 'You are busted! Dealer wins!',
-        });
-      } else {
-        const winnerMessage = checkWinner(player.score, dealer.score);
-        dispatch({ type: 'SET_RESULT', payload: winnerMessage });
-      }
-    }, 1000);
-
-    return () => clearTimeout(timeOutId);
-  }, [dealer, gameOver, player]);
+  useGameResult({ player, dealer, gameOver, dispatch });
 
   if (!player || !dealer)
-    return <h1 className="text-3xl text-center mt-16">Loading...</h1>;
+    return (
+      <div className="text-center mt-16">
+        <h1 className="text-3xl">Loading...</h1>
+        <p className="text-zinc-500">
+          If this takes too long, please refresh the page.
+        </p>
+      </div>
+    );
 
   return (
     <div className="bg-white dark:bg-zinc-800 p-4 flex justify-center items-center flex-col h-screen w-full relative">
+      <Toaster position="top-center" />
       <ThemeToggle theme={theme} setTheme={setTheme} />
       <Header onReset={onReset} stats={stats} onResetStats={onResetStats} />
       <GameBoard
