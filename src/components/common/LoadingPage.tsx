03c9cbc0e3aca2baa917605c6479896b435b9f84
Nikola Kenjic <nikola.nico89@gmail.com>
2025-06-09 20:15:33 +0200

    create loading page

---
+++ src/components/common/LoadingPage.tsx
@@ -0,0 +1,12 @@
+const LoadingPage = () => {
+  return (
+    <div className="text-center mt-16">
+      <h1 className="text-3xl">Loading...</h1>
+      <p className="text-zinc-500">
+        If this takes too long, please refresh the page.
+      </p>
+    </div>
+  );
+};
+
+export default LoadingPage;
